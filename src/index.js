4886c7a4990a26062e637c62c3bb1376d353371e
jbryans <jbryans2014@gmail.com>
2022-06-26 15:21:42 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // import axios here
+const { default: Axios } = require('axios')
+const axios = require('axios')
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -8,17 +10,33 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
-}
+ return Axios(url)
+ .then()
+ 
 
+}
+// discoverMovie()
+// console.log(discoverMovie('Resivior Dogs'))
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+ 
+  return Axios(url)
+  .then((id) => {return id.data})
+
 }
+getMovieById(400)
+
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return Axios(url)
+  .then((response) => {return response})
+  .catch((error) => {
+    return error.response.status
+  })
 }
 
 
